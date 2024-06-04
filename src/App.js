ad339ea48708986183e7a0432efd9574b08288e6
Mohan Rao <mohan.rao@techsaraz.in>
2024-06-04 19:30:26 +0530

    lms design created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Services from './Components/Services';
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Services/>
+
+   </>
+  );
+}
+
+export default App;
---
+++ src/Components/Services.js
@@ -0,0 +1,158 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <>
+    {/* next section start */}
+    <div className="max-w-[1320px] md:py-[80] py-5 mt-5 mx-auto flex sm:flex-row flex-col">
+        <div className="basis-[45%] pb-5">
+            <img src="	https://www.wscubetech.com/images/e-learn-center.webp" alt="" className="w-full"/>
+        </div>
+        <div className="basis-[55%] px-5">
+            <h3 className=" pb-5">Tech Saraz is a software company based in Bangalore that specializes in top-notch digital Marketing and web design. In our fast-paced digital world, it’s crucial to make your products or services shine brighter than the rest. We’re here to support those with big dreams and a relentless drive to succeed in their online businesses by providing our unmatched services.</h3>
+            <p className='py-3'>A well-designed website is the key to drawing in and keeping customers in this digital era. We at Tech Saraz grasp this essential fact, and our work ranges from creating the most inventive and user-friendly websites to effectively marketing business online.</p>
+            <p className="py-3">A top-tier website and extraordinary marketing services can act as the spark that sends business soaring. Our experienced professionals are committed to ensuring your online presence isn’t just another face in the crowd but a standout player that grabs attention and fuels business growth. With Tech Saraz, you can be confident that your digital marketing needs are in the safest hands.</p>
+        </div>
+    </div>
+    {/* next section closed */}
+          <div className="max-w-[1320px] md:py-[80] py-5 mx-auto">
+        <h1 className="text-4xl text-center">Our Services</h1>
+    </div>
+    <div className="max-w-[1200px] mx-auto py-[30px] grid lg:grid-cols-4 sm:grid-cols-2 gap-6">
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-spin"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-bounce"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-bounce"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-spin"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-bounce"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-bounce"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-bounce"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+        <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
+            <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-bounce"></i>
+            <h3 className="text-xl py-2">Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
+        </div>
+    </div>
+
+    {/* next section start */}
+    <div className="max-w-[1320px] mx-auto mt-5 ">
+        <h1 className="text-4xl text-center py-5 mb-3">Featured Products</h1>
+    </div>
+    <div className="max-w-[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px]">
+        <div className="text-center shadow-lg rounded">
+            <div className="overflow-hidden">
+                <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
+            </div>
+            <h3 className="py-2 text-2xl">web design</h3>
+            <p className="py-2">RS 8000 </p>
+        </div>
+        <div className="text-center shadow-lg rounded">
+            <div className="overflow-hidden">
+                <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
+            </div>
+            <h3 className="py-2 text-2xl">Digital Marketing</h3>
+            <p className="py-2">RS 48000 </p>
+        </div>
+        <div className="text-center shadow-lg rounded">
+            <div className="overflow-hidden">
+                <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
+            </div>
+            <h3 className="py-2 text-2xl">Web Development</h3>
+            <p className="py-2">RS 58000 </p>
+        </div>
+        <div className="text-center shadow-lg rounded">
+            <div className="overflow-hidden">
+                <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
+            </div>
+            <h3 className="py-2 text-2xl">Graphic Designing</h3>
+            <p className="py-2">RS 28000 </p>
+        </div>
+    </div>
+
+    {/* next section closed */}
+
+    {/* next section start */}
+    {/* <div className="max-w-[1320px] mx-auto  ">
+        <h1 className="text-4xl text-center py-5 mb-3">Questions? Call 000-800-919-1694</h1>
+    </div> */}
+    <div className="max-w-[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px] py-[50px] mt-[150px] bg-blue-800">
+    
+      <div>
+                        <ul class="text-[#ccc]">
+                            <li class="mb-[10px]"><a href="#" class="underline">Faq</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">Investor Relations</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">Privacy</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">Speed Test</a></li>
+                            
+                        </ul>
+      </div>
+      <div>
+                        <ul class="text-[#ccc]">
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Help Centre</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Jobs</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Cookie Preferences</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Legal Notices</a></li>
+                            
+                        </ul>
+      </div>
+      <div>
+                        <ul class="text-[#ccc]">
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Account</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Ways to Watch</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Corporate Information</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Only on Netflix</a></li>
+                            
+                        </ul>
+      </div>
+      <div>
+                        <ul class="text-[#ccc]">
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Media Centre</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">Terms of Use</a></li>
+                            <li class="mb-[10px]"><a href="#" class="underline">
+                                Contact Us
+                                </a></li>
+                            
+                        </ul>
+      </div>
+    </div>
+    
+    {/* section closed */}
+    </>
+  )
+}
