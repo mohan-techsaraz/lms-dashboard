86717d147d7485ac56e921679467aeb9cfceae92
Mohan Rao <mohan.rao@techsaraz.in>
2024-06-11 20:21:23 +0530

    new faq section has added and some modification has done

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -4,7 +4,8 @@ export default function Services() {
   return (
     <>
     {/* next section start */}
-    <div className="max-w-[1320px] md:py-[80] py-5 mt-5 mx-auto flex sm:flex-row flex-col">
+    <section className='w-[100%]'>
+    <div className="max-w-[1170px] md:py-[80] py-5 mt-5 mx-auto flex sm:flex-row flex-col">
         <div className="basis-[45%] pb-5">
             <img src="	https://www.wscubetech.com/images/e-learn-center.webp" alt="" className="w-full"/>
         </div>
@@ -14,11 +15,15 @@ export default function Services() {
             <p className="py-3">A top-tier website and extraordinary marketing services can act as the spark that sends business soaring. Our experienced professionals are committed to ensuring your online presence isn’t just another face in the crowd but a standout player that grabs attention and fuels business growth. With Tech Saraz, you can be confident that your digital marketing needs are in the safest hands.</p>
         </div>
     </div>
+    </section>
+  
     {/* next section closed */}
-          <div className="max-w-[1320px] md:py-[80] py-5 mx-auto">
+    {/* next section start */}
+    <section className='w-[100%]'>
+    <div className="max-w-[1170px] md:py-[80] py-5 mx-auto">
         <h1 className="text-4xl text-center">Our Services</h1>
     </div>
-    <div className="max-w-[1200px] mx-auto py-[30px] grid lg:grid-cols-4 sm:grid-cols-2 gap-6">
+    <div className="max-w-[1170px] mx-auto py-[30px] grid lg:grid-cols-4 sm:grid-cols-2 gap-6">
         <div className="group shadow-lg p-4 text-center hover:bg-blue-500 hover:text-white duration-1000">
             <i className="fas fa-laptop-code text-2xl group-hover:text-white text-blue-500 duration-1000 animate-spin"></i>
             <h3 className="text-xl py-2">Web Design</h3>
@@ -60,34 +65,39 @@ export default function Services() {
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda voluptatum nobis nemo?</p>
         </div>
     </div>
+    </section>
+   
+    {/* closed */}
+  
 
     {/* next section start */}
-    <div className="max-w-[1320px] mx-auto mt-5 ">
+    <section className='w-[100%]'>
+    <div className="max-w-[1170px] mx-auto mt-5 ">
         <h1 className="text-4xl text-center py-5 mb-3">Featured Products</h1>
     </div>
-    <div className="max-w-[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px]">
-        <div className="text-center shadow-lg rounded">
+    <div className="max-w-[1170px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px]">
+        <div className="text-center shadow-lg rounded bg-[red] ">
             <div className="overflow-hidden">
                 <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
             </div>
             <h3 className="py-2 text-2xl">web design</h3>
             <p className="py-2">RS 8000 </p>
         </div>
-        <div className="text-center shadow-lg rounded">
+        <div className="text-center shadow-lg rounded bg-[red]">
             <div className="overflow-hidden">
                 <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
             </div>
             <h3 className="py-2 text-2xl">Digital Marketing</h3>
             <p className="py-2">RS 48000 </p>
         </div>
-        <div className="text-center shadow-lg rounded">
+        <div className="text-center shadow-lg rounded bg-[red]">
             <div className="overflow-hidden">
                 <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
             </div>
             <h3 className="py-2 text-2xl">Web Development</h3>
             <p className="py-2">RS 58000 </p>
         </div>
-        <div className="text-center shadow-lg rounded">
+        <div className="text-center shadow-lg rounded bg-[red]">
             <div className="overflow-hidden">
                 <img src="https://techsaraz.com/wp-content/uploads/2023/04/About-us-new.webp" className="duration-1000 hover:scale-125" alt=""/>
             </div>
@@ -95,64 +105,160 @@ export default function Services() {
             <p className="py-2">RS 28000 </p>
         </div>
     </div>
+    </section>
+  
 
     {/* next section closed */}
 
-    {/* next section start */}
-    {/* <div className="max-w-[1320px] mx-auto  ">
-        <h1 className="text-4xl text-center py-5 mb-3">Questions? Call 000-800-919-1694</h1>
-    </div> */}
-    <div className="max-w-[1320px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px] py-[50px] mt-[150px] bg-blue-800">
+  {/* next section start */}
+  <section className='w-[100%]'>
+  <div className="max-w-[1170px] mx-auto mt-5 ">
+      <h2 className="sm:text-[50px]  text-[40px] font-bold  text-center">Frequently Asked Questions</h2>
+      <div>
+      <div class="py-[30px]">
+                    <div class="bg-[#2d2d2d] mb-[15px]">
+
+                        <div class="p-[20px] text-white sm:text-[30px] text-[20px] relative border-b-[2px] border-[black] hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer faqQuestion"><h3>What is Webdesign?</h3><span class="absolute right-[20px] top-[15px]">+</span></div>
+
+                        <div class="text-white text-[20px]  scale-y-0 h-[0px] origin-top duration-300"><p>Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries and more – on thousands of internet-connected devices.</p>
+
+                            <p>You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!</p></div>
+                    </div>
+                    <div class="bg-[#2d2d2d] mb-[15px]">
+
+                        <div class="p-[20px] text-white sm:text-[30px] text-[20px] relative border-b-[2px] border-[black] hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer faqQuestion"><h3>How much does Web Development cost?</h3><span class="absolute right-[20px] top-[15px]">+</span></div>
+
+                        <div class="text-white text-[20px]  scale-y-0 h-[0px] origin-top duration-300"><p>Watch Netflix on your smartphone, tablet, Smart TV, laptop, or streaming device, all for one fixed monthly fee. Plans range from ₹149 to ₹649 a month. No extra costs, no contracts.</p>
+
+                            <p>You can watch as much as you want, whenever you want, without a single ad – all for one low monthly price. There's always something new to discover, and new TV shows and movies are added every week!</p></div>
+                    </div>
+                    <div class="bg-[#2d2d2d] mb-[15px]">
+
+                        <div class="p-[20px] text-white sm:text-[30px] text-[20px]  relative border-b-[2px] border-[black] hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer faqQuestion"><h3>Where can I watch?</h3><span class="absolute right-[20px] top-[15px]">+</span></div>
+
+                        <div class="text-white text-[20px] scale-y-0 h-[0px] origin-top duration-300"><p>Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.</p>
+
+                            <p>You can also download your favourite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.</p></div>
+                    </div>
+                    <div class="bg-[#2d2d2d] mb-[15px]">
+
+                        <div class="p-[20px] text-white sm:text-[30px] text-[20px] relative border-b-[2px] border-[black] hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer faqQuestion"><h3>How do I cancel?</h3><span class="absolute right-[20px] top-[15px]">+</span></div>
+
+                        <div class="text-white text-[20px]  scale-y-0 h-[0px] origin-top duration-300"><p>Netflix is flexible. There are no annoying contracts and no commitments. You can easily cancel your account online in two clicks. There are no cancellation fees – start or stop your account anytime..</p>
+
+                           </div>
+                    </div>
+                    <div class="bg-[#2d2d2d] mb-[15px]">
+
+                        <div class="p-[20px] text-white sm:text-[30px] text-[20px] relative border-b-[2px] border-[black] hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer faqQuestion"><h3>What can I watch on Netflix?</h3><span class="absolute right-[20px] top-[15px]">+</span></div>
+
+                        <div class="text-white text-[20px]  scale-y-0 h-[0px] origin-top duration-300"><p>Netflix has an extensive library of feature films, documentaries, TV shows, anime, award-winning Netflix originals, and more. Watch as much as you want, anytime you want.</p>
+
+                           </div>
+                    </div>
+                    <div class="bg-[#2d2d2d] mb-[15px]">
+
+                        <div class="p-[20px] text-white sm:text-[30px] text-[20px] relative border-b-[2px] border-[black] hover:bg-[rgb(78,78,78)] duration-300 cursor-pointer faqQuestion"><h3>Is Netflix good for kids?</h3><span class="absolute right-[20px] top-[15px]">+</span></div>
+
+                        <div class="text-white text-[20px]  scale-y-0 h-[0px] origin-top duration-300"><p>The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and films in their own space.</p>
+
+                            <p>Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.</p></div>
+                    </div>
+                </div>
+                <p class="text-[20px] text-center text-white">Ready to watch? Enter your email to create or restart your membership.</p>
+
+                <div class="max-w-[700px] mx-auto  sm:px-[10px] px-[40px] mt-[30px]">
+                    <form action="" class="grid  gap-[20px] sm:grid-cols-[70%_auto] sm:grid-cols-1">
+                        <input type="text" class="border-[1px] pl-[20px] h-[50px] border-[#ccc] bg-[transparent]" placeholder="Email Address"/>
+                        <button class="bg-[red] px-[10px] rounded-[5px] text-white py-[7px] hover:bg-[rgb(193, 17, 25)] duration-300 text-[25px]">Get Started </button>
+                    </form>
+                </div>
+      </div>
+
+
+  </div>
+  </section>
+ 
+  {/* next section closed */}
+
+  <section className='w-[100%]'>
+  <div className="max-w-[1170px] mx-auto grid lg:grid-cols-4 md:grid-cols-2 gap-6 px-[20px] py-[50px] mt-[150px] bg-[#2d2d2d]">
     
-      <div>
-                        <ul class="text-[#ccc]">
-                            <li class="mb-[10px]"><a href="#" class="underline">Faq</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">Investor Relations</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">Privacy</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">Speed Test</a></li>
-                            
-                        </ul>
-      </div>
-      <div>
-                        <ul class="text-[#ccc]">
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Help Centre</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Jobs</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Cookie Preferences</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Legal Notices</a></li>
-                            
-                        </ul>
-      </div>
-      <div>
-                        <ul class="text-[#ccc]">
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Account</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Ways to Watch</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Corporate Information</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Only on Netflix</a></li>
-                            
-                        </ul>
-      </div>
-      <div>
-                        <ul class="text-[#ccc]">
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Media Centre</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">Terms of Use</a></li>
-                            <li class="mb-[10px]"><a href="#" class="underline">
-                                Contact Us
-                                </a></li>
-                            
-                        </ul>
-      </div>
+    <div>
+                      <ul class="text-[#ccc]">
+                          <li class="mb-[10px]"><a href="#" class="underline">Faq</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">Investor Relations</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">Privacy</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">Speed Test</a></li>
+                          
+                      </ul>
     </div>
+    <div>
+                      <ul class="text-[#ccc]">
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Help Centre</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Jobs</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Cookie Preferences</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Legal Notices</a></li>
+                          
+                      </ul>
+    </div>
+    <div>
+                      <ul class="text-[#ccc]">
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Account</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Ways to Watch</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Corporate Information</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Only on Netflix</a></li>
+                          
+                      </ul>
+    </div>
+    <div>
+                      <ul class="text-[#ccc]">
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Media Centre</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">Terms of Use</a></li>
+                          <li class="mb-[10px]"><a href="#" class="underline">
+                              Contact Us
+                              </a></li>
+                          
+                      </ul>
+    </div>
+  </div>
+  </section>
+  
     
     {/* section closed */}
     </>
   )
 }
+// faq js start
+{
+    let allQuestion=document.querySelectorAll(".faqQuestion");
+    // console.log(allQuestion);
+    allQuestion.forEach((value,index)=>{
+        // console.log(value)
+        value.addEventListener("click",()=>{
+            value.nextElementSibling.classList.toggle("h-[auto]")
+            console.log(index)
+            value.nextElementSibling.classList.toggle("scale-y-100")
+            value.nextElementSibling.classList.toggle("p-[20px]")
+            value.children[1].classList.toggle("rotate-45")
+
+            allQuestion.forEach((el,i)=>{
+                if(i!=index){
+                    el.nextElementSibling.classList.remove("h-[auto]","scale-y-100","p-[20px]")
+                    el.children[1].classList.remove("rotate-45")
+                }
+            })
+        })
+    })
+}
+
+// faq js closed
